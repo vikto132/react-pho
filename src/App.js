ea99af0b38ff9936c46f5679e6b470e3615182c7
vikto132 <60517065+vikto132@users.noreply.github.com>
2022-12-08 01:54:06 +0700

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import FormCustom from './form';
 
 export default function App() {
   return (
     <div>
+      <FormCustom />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
